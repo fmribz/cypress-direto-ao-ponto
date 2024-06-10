b9068e3b7f88b6651299fa472a6b6c90e252a3f5
Fábio de Moraes Ribeiro <fmribeiro.info@gmail.com>
2024-06-09 21:32:54 -0300

    Asserts
    Adicionando asserções no teste

--- cypress/e2e/finance.cy.js
+++ cypress/e2e/finance.cy.js
@@ -11,8 +11,14 @@ describe('Transações', () => {
         //seleção feita pelo "alvo" do cypress, 'type' insere um texto nesse elemento
         cy.get('#amount').type(20.00);
         //seleção feita pelo "alvo" do cypress, 'type' insere um texto nesse elemento
-        cy.get('#date').type('2024-09-06');
+        cy.get('#date').type('2024-06-09');
         //seleciona um elemento e clica nele
         cy.get('button').click();
+
+        //realização asserts
+        //recomendado ver a documentação em cypress.io
+        cy.get('.description').should('have.text', 'Barra');
+        //cy.get('.income').should('have.text', 'R$ 20,00');
+        cy.get('.date').should('have.text','09/06/2024');
     });
 });
